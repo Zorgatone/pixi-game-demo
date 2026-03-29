7d0f957e031b78c345547ef43e885a768ce1c38f
Tommaso Ricci <zorgatone@gmail.com>
2026-03-29 19:14:05 +0200

    Build new version

--- docs/assets/BufferResource-K34nXIaK.js
+++ docs/assets/BufferResource-K34nXIaK.js
@@ -1,4 +1,4 @@
-import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-BGWIt0pv.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-CRNs3oh2.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
